6b5e6a7fd36427823077a065d5c5c3cdc9a51f09
viktor.brillati <vbrillati@gmail.com>
2022-11-23 22:34:22 -0500

    added all files for cfb site

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require('body-parser');
+const port = 3000;
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.static("public/images"));
+
+app.get('/', (req,res) => {
+    res.render('main');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/volunteer', (req,res) => {
+    res.render('volunteer');
+});
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
